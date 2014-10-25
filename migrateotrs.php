3ef043ba641e1f0b90cf2151482fbc678c56adce
Brady Owens <info@fastglass.net>
2014-10-25 12:44:22 +0200

    Altering the database to process these changes to the OTRS tables

---
+++ migrateotrs.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file Main execute file.
+ */
+global $databases;
+// Require the configuration file.
+require_once(dirname(__FILE__) . '/includes/config.php');
+// Load the database include file and also load the driver file set in the config
+require_once(dirname(__FILE__) . '/includes/database/database.inc');
+require_once dirname(__FILE__) . '/includes/database/' . $databases['default']['default']['driver'] . '/database.inc';
+
+
+// Create an Freshdesk updated or not field for cron processing.
+$spec1 = array(
+  'description' => 'Processed ticket to Freshdesk.',
+  'type' => 'int',
+  'size' => 'normal',
+  'not null' => TRUE,
+);
+// Create an Freshdesk ticket ID field.
+$spec2 = array(
+  'description' => 'Freshdesk ID of ticket.',
+  'type' => 'int',
+  'size' => 'normal',
+  'not null' => TRUE,
+);
+
+// Add fields to the tables needed.
+db_add_field('tickets', 'freshdesk_updated', $spec1);
+db_add_field('tickets', 'freshdesk_id', $spec2);
+db_add_field('article', 'freshdesk_updated', $spec1);
+
+
