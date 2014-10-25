37a9983ab59413ae11c0a1f227f1e9698e5983fa
Brady Owens <info@fastglass.net>
2014-10-25 16:02:54 +0200

    Functions file with a text translation function that is needed to user Drupal DB layer.

---
+++ includes/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file Functions file for migration.
+ */
+
+/**
+ * Fake function to allow the Drupal DB Abstraction layer to work.
+ */
+function t($string, array $args = array(), array $options = array()) {
+  return $string;
+}
