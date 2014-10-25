ca9f07b9db44d97ea7995b7c20f2579b34236719
Brady Owens <info@fastglass.net>
2014-10-25 16:04:11 +0200

    More includes to get DB layer working right and start processing base tickets.

--- migrateotrs.php
+++ migrateotrs.php
@@ -6,6 +6,10 @@
 global $databases;
 // Require the configuration file.
 require_once(dirname(__FILE__) . '/includes/config.php');
+// Load the functions file.
+require_once(dirname(__FILE__) . '/includes/functions.php');
+// Load text functions needed for DB layer.
+require_once(dirname(__FILE__) . '/includes/unicode.inc');
 // Load the database include file and also load the driver file set in the config
 require_once(dirname(__FILE__) . '/includes/database/database.inc');
 require_once dirname(__FILE__) . '/includes/database/' . $databases['default']['default']['driver'] . '/database.inc';
@@ -15,28 +19,36 @@ $spec1 = array(
   'description' => 'Processed ticket to Freshdesk.',
   'type' => 'int',
   'size' => 'normal',
-  'not null' => TRUE,
+  'default' => 0,
 );
 // Create an Freshdesk ticket ID field.
 $spec2 = array(
   'description' => 'Freshdesk ID of ticket.',
   'type' => 'int',
   'size' => 'normal',
-  'not null' => TRUE,
+  'default' => 0,
 );
 
 // Add fields to the tables needed.
-if (!db_field_exists('tickets', 'freshdesk_updated')) {
-  db_add_field('tickets', 'freshdesk_updated', $spec1);
+if (!db_field_exists('ticket', 'freshdesk_updated')) {
+  db_add_field('ticket', 'freshdesk_updated', $spec1);
 }
 // Add fields to the tables needed.
-if (!db_field_exists('tickets', 'freshdesk_id')) {
-  db_add_field('tickets', 'freshdesk_id', $spec2);
+if (!db_field_exists('ticket', 'freshdesk_id')) {
+  db_add_field('ticket', 'freshdesk_id', $spec2);
 }
 // Add fields to the tables needed.
 if (!db_field_exists('article', 'freshdesk_updated')) {
   db_add_field('article', 'freshdesk_updated', $spec1);
 }
 
+// First process all base tickets till they are exhausted
+$query = 'SELECT id, title FROM {ticket} WHERE freshdesk_updated = 0 ORDER by id LIMIT ' . $settings['chunksize'];
+$result = db_query($query);
+if ($result->rowCount() != 0) {
+  foreach ($result as $item){
+    $articleresult = db_query('SELECT id, a_body, a_from, a_reply_to FROM {article} WHERE ticket_id = ' . $item->id . ' ORDER BY id');
+  }
+}
 
 ?>
